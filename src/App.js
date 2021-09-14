612ba6a4f9dc188c5f5afabf977bd8d16928e3bc
Dimitar Nikolov <dknikolov@gmail.com>
2021-09-14 16:41:52 +0300

    feat: add textarea skeleton

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
+      <div className="box">
+        <div className="field">
+          <div className="control">
+            <textarea className="textarea is-large" placeholder="Notes..." />
+          </div>
         </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
+        <button className="button is-large is-primary is-active">Save</button>
+        <button className="button is-large">Clear</button>
       </div>
     </div>
   );
